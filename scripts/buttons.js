a751041e74bccce5ea1e07f003c9ad447f73da52
Romasssssa <715075@pdsb.net>
2020-10-28 11:38:15 -0700

    buttons

---
+++ scripts/buttons.js
@@ -0,0 +1,40 @@
+function greet(){
+    alert("Hi! Welcome to my website");
+  }
+  
+  function getName(){
+    let name = prompt("What's your name?");
+    alert("Nice to meet you, " + name);
+  }
+
+  function func1(){
+    alert("my final seminar will be here soon!!");
+  }
+
+  function func2(){
+    alert("my ethics assignments will be here soon!!");
+  }
+
+  function func3(){
+    alert(" all my other assignments will be here soon!!");
+  }
+
+  function func4(){
+    alert(" all my other assignments will be here soon!!");
+  }
+
+  function func5(){
+    alert("have a couple questions here");
+  }
+
+  function func6(){
+    alert(" question 1");
+  }
+
+  function func7(){
+    alert(" question 1");
+  }
+
+  function func8(){
+    alert(" question 1");
+  }
