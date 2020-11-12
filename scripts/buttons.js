f41b4b5c8e9cbfd73cd4e154a160057617fe671e
Romasssssa <715075@pdsb.net>
2020-11-12 15:46:28 -0800

    I AM DONE

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -23,15 +23,6 @@ function greet(){
     alert("all my other assignments will be here soon!!");
   }
 
-
-  function func5(){
-    alert(" all my other assignments will be here soon!!");
-  }
-
-  function func6(){
-    alert(" all my other assignments will be here soon!!");
-  }
-
   function func7(){
     alert(" all my other assignments will be here soon");
   }
@@ -59,33 +50,31 @@ function greet(){
   }
   
 function encrypt (){ 
-let plaintext = prompt ("enter your message ");
-let ciphertext = "";
-let firstletter= "";
-plaintext = plaintext.split("");
-for (let i = 0 ; i< plaintext.length; i = i + 1){
- firstletter=(plaintext[i][0]).repeat(2);
-  
-  ciphertext += "oogey" + (plaintext[i]).slice(1,-1) + firstletter + "YOLO";
+let plaintext = prompt ("enter your message "); //user will be writing down the message they want to encrypt
+let ciphertext = " ";
+let firstletter= " ";
+plaintext = plaintext.split(" ");
+for (let i = 0 ; i< plaintext.length; i = i + 1){ //this is the loop
+ firstletter=(plaintext[i][0]).repeat(3); // how many times i want the phrase to repeat
+  ciphertext += "oogey" + (plaintext[i]).slice(0) + firstletter + "YOLO "; // how the encryption format is
 }
-
 document.getElementById("encrypted").innerHTML=ciphertext;
+
 // var str = document.getElementById("encrypted").innerHTML=txt;
-alert("here is your encrypted message  " + ciphertext)
+alert("here is your encrypted message  " + ciphertext) // what the user will recive
 }
 
- function decrypt(){
-   let ciphertext = prompt ("Enter your encrypted message here ");
+ function decrypt(){ // the decryption part
+   let ciphertext = prompt ("Enter your encrypted message here "); // recieving decrypted message
    let plaintext = "";
-  ciphertext = ciphertext.split("");
-  for (let i = 0 ; i< plaintext.length; i = i + 1){
-    let firstletter = (ciphertext [i]).slice(0,-2).repeat();
-    let sentence= (ciphertext[i]).slice(0).split();
-    let replace = ("YOLO", "");
-    plaintext += firstletter + (ciphertext[i]).slice(0) + sentence + "" + replace;
-    
+  ciphertext = ciphertext.split(" ");
+  for (let i = 0 ; i < ciphertext.length; i = i + 1){ // the decryption loop
+    plaintext += (ciphertext[i]).slice(5,-7);
+
   }
-   
-alert("here is your decrypted message" + plaintext)
   document.getElementById("decrypted").innerHTML=plaintext;
+alert("here is your decrypted message " + plaintext); //what the user will recive
+  
   }
+// YAAYYYY I DID IT (after struggling) BUT IT LOOKS SO COOL
+//credits to Joseph and Baasim for helping/editing
