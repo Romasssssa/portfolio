d5bb7e2fe6e5402fdc6aee5a945331c2fdadcc59
Romasssssa <715075@pdsb.net>
2020-11-11 19:47:58 -0800

    encryption assignment

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -48,4 +48,44 @@ function greet(){
     alert("give some feedback");
   }
 
+  function whileSumsThrees(){
+    let sum= 0;
+    let i=0;
+    while (i < 1000) {
+      sum+=i;
+      i = i + 3;
+    }
+    console.log(sum);
+  }
   
+function encrypt (){ 
+let plaintext = prompt ("enter your message ");
+let ciphertext = "";
+let firstletter= "";
+plaintext = plaintext.split("");
+for (let i = 0 ; i< plaintext.length; i = i + 1){
+ firstletter=(plaintext[i][0]).repeat(2);
+  
+  ciphertext += "oogey" + (plaintext[i]).slice(1,-1) + firstletter + "YOLO";
+}
+
+document.getElementById("encrypted").innerHTML=ciphertext;
+// var str = document.getElementById("encrypted").innerHTML=txt;
+alert("here is your encrypted message  " + ciphertext)
+}
+
+ function decrypt(){
+   let ciphertext = prompt ("Enter your encrypted message here ");
+   let plaintext = "";
+  ciphertext = ciphertext.split("");
+  for (let i = 0 ; i< plaintext.length; i = i + 1){
+    let firstletter = (ciphertext [i]).slice(0,-2).repeat();
+    let sentence= (ciphertext[i]).slice(0).split();
+    let replace = ("YOLO", "");
+    plaintext += firstletter + (ciphertext[i]).slice(0) + sentence + "" + replace;
+    
+  }
+   
+alert("here is your decrypted message" + plaintext)
+  document.getElementById("decrypted").innerHTML=plaintext;
+  }
